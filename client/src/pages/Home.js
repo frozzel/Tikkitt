d93eb01a4b5bc50395dcff7abe1fad6febc2297e
Dennis H <111554091+frozzel@users.noreply.github.com>
2022-12-01 10:44:20 -0500

    added abigail and some Ui fixes

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -5,6 +5,7 @@ import colab from '../images/colab.png'
 import Assign from '../images/Assign.png'
 import 'animate.css';
 import Dennis from '../images/dennis.png'
+import Abigail from '../images/abigail.png'
 import Jessica from '../images/jessica.png'
 import Joshua from '../images/Joshua.png'
 import {
@@ -129,10 +130,10 @@ const Home = () => {
             <div className="card p-4"> 
               <div className=" image d-flex flex-column justify-content-center align-items-center">   
                 <button className="btn btn-secondary"> 
-                  <img src={Dennis} alt="dennis head shot"height="100" width="100" />  
-                    </button> <span className="name mt-3">Dennis Hickox</span> <span className="idd">frozzel@me.com</span> 
+                  <img src={Abigail} alt="dennis head shot"height="100" width="100" />  
+                    </button> <span className="name mt-3">Abigail LeConte</span> <span className="idd">aeleconte21@gmail.com</span> 
                       <div className="d-flex flex-row justify-content-center align-items-center gap-2"> <span className="idd1">MERN Developer</span></div> 
-                            <div className="text mt-3">Project Contributions:<br /><br />MongoDb, Server and React setup. UI animations. Create and Read functions from client side to DB. React Beautiful Dnd deployment! </div> 
+                            <div className="text mt-3">Project Contributions:<br /><br />Wrote out README file & connected AWS email server so generate email notification within application</div> 
                              </div></div>
             </div>
 
