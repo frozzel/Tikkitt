5cae99ad72895297facd0eab2e3b23aee8ed09a8
Dennis H <111554091+frozzel@users.noreply.github.com>
2022-11-30 21:15:43 -0500

    dev team cards in place

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -4,6 +4,7 @@ import projectM from '../images/projectM.png'
 import colab from '../images/colab.png'
 import Assign from '../images/Assign.png'
 import 'animate.css';
+import Dennis from '../images/dennis.png'
 
 
 
@@ -74,6 +75,66 @@ const Home = () => {
               <h1>Assign Tikkitt's with due dates & notifications!</h1>
             </div>
           
+        </div>
+
+        <div className='sect4 flex-row'>
+        <div className='Text4 col-12'><h1>Meet Our Development Team!</h1></div>
+        <div className='flex DevCards'>
+        <div className="container mt-4 mb-4 p-3 d-flex justify-content-center"> 
+            <div className="card p-4"> 
+              <div className=" image d-flex flex-column justify-content-center align-items-center">   
+                <button className="btn btn-secondary"> 
+                  <img src={Dennis} alt="dennis head shot"height="100" width="100" />  
+                    </button> <span className="name mt-3">Dennis Hickox</span> <span className="idd">frozzel@me.com</span> 
+                      <div className="d-flex flex-row justify-content-center align-items-center gap-2"> <span className="idd1">MERN Developer</span></div> 
+                        {/* <div className="d-flex flex-row justify-content-center align-items-center mt-3"> <span className="number">1069 <span className="follow">Followers</span></span> </div>  */}
+                          {/* <div className=" d-flex mt-2"> <button className="btn1 btn-dark">Edit Profile</button> </div>  */}
+                            <div className="text mt-3">Project Contributions:<br /><br />MongoDb, Server and React setup. UI animations. Create and Read functions from client side to DB. React Beautiful Dnd deployment! </div> 
+                             </div></div>
+            </div>
+
+            <div className="container mt-4 mb-4 p-3 d-flex justify-content-center"> 
+            <div className="card p-4"> 
+              <div className=" image d-flex flex-column justify-content-center align-items-center">   
+                <button className="btn btn-secondary"> 
+                  <img src={Dennis} alt="dennis head shot"height="100" width="100" />  
+                    </button> <span className="name mt-3">Dennis Hickox</span> <span className="idd">frozzel@me.com</span> 
+                      <div className="d-flex flex-row justify-content-center align-items-center gap-2"> <span className="idd1">MERN Developer</span></div> 
+                        {/* <div className="d-flex flex-row justify-content-center align-items-center mt-3"> <span className="number">1069 <span className="follow">Followers</span></span> </div>  */}
+                          {/* <div className=" d-flex mt-2"> <button className="btn1 btn-dark">Edit Profile</button> </div>  */}
+                            <div className="text mt-3">Project Contributions:<br /><br />MongoDb, Server and React setup. UI animations. Create and Read functions from client side to DB. React Beautiful Dnd deployment! </div> 
+                             </div></div>
+            </div>
+
+            <div className="container mt-4 mb-4 p-3 d-flex justify-content-center"> 
+            <div className="card p-4"> 
+              <div className=" image d-flex flex-column justify-content-center align-items-center">   
+                <button className="btn btn-secondary"> 
+                  <img src={Dennis} alt="dennis head shot"height="100" width="100" />  
+                    </button> <span className="name mt-3">Dennis Hickox</span> <span className="idd">frozzel@me.com</span> 
+                      <div className="d-flex flex-row justify-content-center align-items-center gap-2"> <span className="idd1">MERN Developer</span></div> 
+                        {/* <div className="d-flex flex-row justify-content-center align-items-center mt-3"> <span className="number">1069 <span className="follow">Followers</span></span> </div>  */}
+                          {/* <div className=" d-flex mt-2"> <button className="btn1 btn-dark">Edit Profile</button> </div>  */}
+                            <div className="text mt-3">Project Contributions:<br /><br />MongoDb, Server and React setup. UI animations. Create and Read functions from client side to DB. React Beautiful Dnd deployment! </div> 
+                             </div></div>
+            </div>
+
+            <div className="container mt-4 mb-4 p-3 d-flex justify-content-center"> 
+            <div className="card p-4"> 
+              <div className=" image d-flex flex-column justify-content-center align-items-center">   
+                <button className="btn btn-secondary"> 
+                  <img src={Dennis} alt="dennis head shot"height="100" width="100" />  
+                    </button> <span className="name mt-3">Dennis Hickox</span> <span className="idd">frozzel@me.com</span> 
+                      <div className="d-flex flex-row justify-content-center align-items-center gap-2"> <span className="idd1">MERN Developer</span></div> 
+                        {/* <div className="d-flex flex-row justify-content-center align-items-center mt-3"> <span className="number">1069 <span className="follow">Followers</span></span> </div>  */}
+                          {/* <div className=" d-flex mt-2"> <button className="btn1 btn-dark">Edit Profile</button> </div>  */}
+                            <div className="text mt-3">Project Contributions:<br /><br />MongoDb, Server and React setup. UI animations. Create and Read functions from client side to DB. React Beautiful Dnd deployment! </div> 
+                             </div></div>
+            </div>
+
+        </div>
+
+
         </div>
 
     </main>
